1364eeeb037a2b7e9f400537970c923201ea022c
singhvichirag <chirags@iitk.ac.in>
2017-07-06 20:27:09 +0530

    Map Paths

---
+++ app/src/main/java/Modules/Duration.java
@@ -0,0 +1,14 @@
+package Modules;
+
+/**
+ * Created by Chirag  Singhvi on 04-Jul-17.
+ */
+public class Duration {
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
